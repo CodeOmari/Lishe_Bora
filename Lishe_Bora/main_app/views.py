21da7df0ac69639aea4fc3507ec519133846c9c2
CodeOmari <jacksonomari2230@gmail.com>
2025-10-25 14:07:35 +0300

    add permissions to the RecipeViewSet to restrict recipe access and modification

--- Lishe_Bora/main_app/views.py
+++ Lishe_Bora/main_app/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.contrib.auth.models import User
 from .serializers import CustomUserSerializer
 from rest_framework import generics
@@ -7,7 +5,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import IngredientSerializer, RecipeSerializer
 from .models import Recipe, Ingredient
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 # Create your views here.
 class UserCreateView(generics.CreateAPIView):
@@ -15,11 +13,32 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
 
+# Ensures only the author of a recipe can update/delete it   
+class IsRecipeAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+    
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsRecipeAuthorOrReadOnly]
+
+    # Sets the currently logged in user as the owner of the event they created
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    # authors can only see recipes they own
+    # Other users can see all recipes
+    def get_queryset(self):
+        user= self.request.user
+        if Recipe.objects.filter(author=user).exists():
+            return Recipe.objects.filter(author=user)
+        return Recipe.objects.all()
+    
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
